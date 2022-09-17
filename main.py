aaf7ccb37f08a1d27cbfd4ef1148e6513fffb7fc
kaloviev <kaloviev@bk.ru>
2022-09-17 11:49:02 +0300

    Cleanup code

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ chat_id = os.getenv('MARIIN_CHAT_ID')
 url = 'https://mariin.ru/forms/onlineappointment'
 image = 'status.png'
 
-print('!!!!!!!!')
-print(os.getenv('PUPPETEER_EXEC_PATH'))
-
 async def send_status_image():
     print('[INFO] Enter script')
 
     browser = await launch({
-        'executablePath': os.getenv('PUPPETEER_EXEC_PATH')
+        # 'executablePath': os.getenv('PUPPETEER_EXEC_PATH')
     })
     page = await browser.newPage()
     await page.setViewport({'width': 800, 'height': 1000})
