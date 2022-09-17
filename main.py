6a817fecd5a6a2752a1b1a21282c6ab04712f808
kaloviev <kaloviev@bk.ru>
2022-09-17 17:56:41 +0300

    Test

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pyppeteer import launch
 
 # bot_token = os.getenv('MARIIN_BOT_TOKEN')
 # chat_id = os.getenv('MARIIN_CHAT_ID')
-# url = 'https://mariin.ru/forms/onlineappointment'
-# image = 'status.png'
+url = 'https://mariin.ru/forms/onlineappointment'
+image = 'status.png'
 
 print('!!!!!!!')
 print(os.getenv('CHROMIUM_EXEC_PATH'))
@@ -14,14 +14,24 @@ print(os.getenv('CHROMIUM_EXEC_PATH'))
 async def send_status_image():
     print('[INFO] Enter script')
 
+    print('[INFO] Create a browser')
     browser = await launch(
         headless=True,
         executablePath=os.getenv('CHROMIUM_EXEC_PATH')
     )
+    print('[INFO] Create a page')
     page = await browser.newPage()
+
+    print('[INFO] Set viewport size')
     await page.setViewport({'width': 800, 'height': 1000})
+
+    print('[INFO] Go to a page')
     await page.goto(url, { "waitUntil": 'load', "timeout": 0 })
-    await page.screenshot({'path': image})
+
+    print('[INFO] Take a screenshot')
+    await page.screenshot({ 'path': image })
+
+    print('[INFO] Close the browser')
     await browser.close()
 
 #     bot = telegram.Bot(token=bot_token)
