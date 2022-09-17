cefda685575d202f9fb8db95fd70e8b7cb2b0474
kaloviev <kaloviev@bk.ru>
2022-09-17 17:40:18 +0300

    Test

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import os
-import asyncio
-from pyppeteer import launch
-import telegram
+# import asyncio
+# from pyppeteer import launch
+# import telegram
 
-bot_token = os.getenv('MARIIN_BOT_TOKEN')
-chat_id = os.getenv('MARIIN_CHAT_ID')
-url = 'https://mariin.ru/forms/onlineappointment'
-image = 'status.png'
+# bot_token = os.getenv('MARIIN_BOT_TOKEN')
+# chat_id = os.getenv('MARIIN_CHAT_ID')
+# url = 'https://mariin.ru/forms/onlineappointment'
+# image = 'status.png'
 
 print('!!!!!!!')
 print(os.getenv('CHROMIUM_EXEC_PATH'))
 
-async def send_status_image():
-    print('[INFO] Enter script')
+# async def send_status_image():
+#     print('[INFO] Enter script')
 
-    browser = await launch(
-        headless=True,
-        executablePath=os.getenv('CHROMIUM_EXEC_PATH')
-    )
-    page = await browser.newPage()
-    await page.setViewport({'width': 800, 'height': 1000})
-    await page.goto(url, { "waitUntil": 'load', "timeout": 0 })
-    await page.screenshot({'path': image})
-    await browser.close()
+#     browser = await launch(
+#         headless=True,
+#         executablePath=os.getenv('CHROMIUM_EXEC_PATH')
+#     )
+#     page = await browser.newPage()
+#     await page.setViewport({'width': 800, 'height': 1000})
+#     await page.goto(url, { "waitUntil": 'load', "timeout": 0 })
+#     await page.screenshot({'path': image})
+#     await browser.close()
 
-    bot = telegram.Bot(token=bot_token)
-    try:
-        await bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
-    except TypeError: # ToDo: Fix this
-        pass
+#     bot = telegram.Bot(token=bot_token)
+#     try:
+#         await bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
+#     except TypeError: # ToDo: Fix this
+#         pass
 
-    print('[INFO] Exit script')
+#     print('[INFO] Exit script')
 
 if __name__ == '__main__':
     print('[INFO] Run script')
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(send_status_image())
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
+    # loop.run_until_complete(send_status_image())
 
